0e92d72f4c472ec0d583eb0cfc9f84a9320fedba
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2022-09-24 11:28:57 +0200

    start bindings more image/shape accessors

--- lib/nanosvg.mli
+++ lib/nanosvg.mli
@@ -1,8 +1,37 @@
+type line_join = Join_miter | Join_round | Join_bevel
+type line_cap = Cap_butt | Cap_round | Cap_square
+type fill_rule = Fillrule_nonzero | Fillrule_evenodd
+
+module Gradient : sig
+  type t
+end
+
+type paint =
+  | Paint_none
+  | Paint_color of Int32.t
+  | Paint_linear_gradient of Gradient.t
+  | Paint_radial_gradient of Gradient.t
+
+module Shape : sig
+  type t
+  val fill : t -> paint
+  val stroke : t -> paint
+  val opacity : t -> float
+  val stroke_width : t -> float
+  val stroke_dash_offset : t -> float
+  val stroke_dash_count : t -> int
+  val stroke_line_join : t -> line_join
+  val stroke_line_cap : t -> line_cap
+  val miter_limit : t -> float
+  val fill_rule : t -> fill_rule
+end
+
 module Image : sig
   type t
   val width : t -> float
   val height : t -> float
   val delete : t -> unit
+  val shapes : t -> Shape.t list
 end
 
 type units = Px | Pt | Pc | Mm | Cm | In
--- lib/nanosvg_stubs.c
+++ lib/nanosvg_stubs.c
@@ -12,22 +12,6 @@
 #include "nanosvg.h"
 #include "nanosvgrast.h"
 
-value caml_nsvg_image_width(value image) {
-  CAMLparam1(image);
-  CAMLlocal1(ret);
-  NSVGimage* image_p = (NSVGimage*) (image & ~1);
-  ret = caml_copy_double((double)image_p->width);
-  CAMLreturn(ret);
-}
-
-value caml_nsvg_image_height(value image) {
-  CAMLparam1(image);
-  CAMLlocal1(ret);
-  NSVGimage* image_p = (NSVGimage*) (image & ~1);
-  ret = caml_copy_double((double)image_p->height);
-  CAMLreturn(ret);
-}
-
 value caml_nsvg_parse_from_file(value filename, value units, value dpi) {
   CAMLparam3(filename, units, dpi);
   CAMLlocal1(ret);
@@ -57,6 +41,146 @@ value caml_nsvg_parse(value data, value units, value dpi) {
 
 value caml_nsvg_delete(value image) {
   nsvgDelete((NSVGimage*) (image & ~1));
+}
+
+// NSVGimage accessors
+
+value caml_nsvg_image_width(value image) {
+  CAMLparam1(image);
+  CAMLlocal1(ret);
+  NSVGimage* image_p = (NSVGimage*) (image & ~1);
+  ret = caml_copy_double((double)image_p->width);
+  CAMLreturn(ret);
+}
+
+value caml_nsvg_image_height(value image) {
+  CAMLparam1(image);
+  CAMLlocal1(ret);
+  NSVGimage* image_p = (NSVGimage*) (image & ~1);
+  ret = caml_copy_double((double)image_p->height);
+  CAMLreturn(ret);
+}
+
+value caml_nsvg_image_shapes(value image) {
+  CAMLparam1(image);
+  CAMLlocal1(list);
+  list = Val_int(0);
+  value* cur = &list;
+  NSVGimage* image_p = (NSVGimage*) (image & ~1);
+  NSVGshape* shape = image_p->shapes;
+  while (shape) {
+    assert (((uintptr_t) shape & 1) == 0);
+    *cur = caml_alloc_tuple(2);
+    Field(*cur, 0) = ((value) shape | 1);
+    Field(*cur, 1) = Val_int(0);
+    cur = &Field(*cur, 1);
+    shape = shape->next;
+  }
+  CAMLreturn(list);
+}
+
+// NSVGpaint
+
+value caml_nsvg_paint(NSVGpaint* paint) {
+  CAMLparam0();
+  CAMLlocal2(ret, tmp);
+  switch(paint->type) {
+    case NSVG_PAINT_NONE:
+      ret = Val_int(0);
+      break;
+    case NSVG_PAINT_COLOR:
+      ret = caml_alloc(1, 1);
+      tmp = caml_copy_int32(paint->color);
+      Field(ret, 0) = tmp;
+      break;
+    case NSVG_PAINT_LINEAR_GRADIENT:
+      ret = caml_alloc(1, 2);
+      assert (((uintptr_t) paint->gradient & 1) == 0);
+      tmp = (value) paint->gradient | 1;
+      Field(ret, 0) = tmp;
+      break;
+    case NSVG_PAINT_RADIAL_GRADIENT:
+      ret = caml_alloc(1, 3);
+      assert (((uintptr_t) paint->gradient & 1) == 0);
+      tmp = (value) paint->gradient | 1;
+      Field(ret, 0) = tmp;
+      break;
+    default: // impossible
+      ret = Val_int(0);
+      break;
+  }
+  CAMLreturn(ret);
+}
+// NSVGshape accessors
+
+value caml_nsvg_shape_opacity(value shape) {
+  CAMLparam1(shape);
+  CAMLlocal1(ret);
+  NSVGshape* shape_p = (NSVGshape*) (shape & ~1);
+  ret = caml_copy_double((double)shape_p->opacity);
+  CAMLreturn(ret);
+}
+
+value caml_nsvg_shape_stroke_width(value shape) {
+  CAMLparam1(shape);
+  CAMLlocal1(ret);
+  NSVGshape* shape_p = (NSVGshape*) (shape & ~1);
+  ret = caml_copy_double((double)shape_p->strokeWidth);
+  CAMLreturn(ret);
+}
+
+value caml_nsvg_shape_stroke_dash_offset(value shape) {
+  CAMLparam1(shape);
+  CAMLlocal1(ret);
+  NSVGshape* shape_p = (NSVGshape*) (shape & ~1);
+  ret = caml_copy_double((double)shape_p->strokeDashOffset);
+  CAMLreturn(ret);
+}
+
+// TODO: strokeDashArray
+
+value caml_nsvg_shape_stroke_dash_count(value shape) {
+  NSVGshape* shape_p = (NSVGshape*) (shape & ~1);
+  return Val_int(shape_p->strokeDashCount);
+}
+
+value caml_nsvg_shape_stroke_line_join(value shape) {
+  NSVGshape* shape_p = (NSVGshape*) (shape & ~1);
+  return Val_int(shape_p->strokeLineJoin);
+}
+
+value caml_nsvg_shape_stroke_line_cap(value shape) {
+  NSVGshape* shape_p = (NSVGshape*) (shape & ~1);
+  return Val_int(shape_p->strokeLineCap);
+}
+
+value caml_nsvg_shape_miter_limit(value shape) {
+  CAMLparam1(shape);
+  CAMLlocal1(ret);
+  NSVGshape* shape_p = (NSVGshape*) (shape & ~1);
+  ret = caml_copy_double((double)shape_p->miterLimit);
+  CAMLreturn(ret);
+}
+
+value caml_nsvg_shape_fill_rule(value shape) {
+  NSVGshape* shape_p = (NSVGshape*) (shape & ~1);
+  return Val_int(shape_p->fillRule);
+}
+
+value caml_nsvg_shape_fill(value shape) {
+  CAMLparam1(shape);
+  CAMLlocal1(ret);
+  NSVGshape* shape_p = (NSVGshape*) (shape & ~1);
+  ret = caml_nsvg_paint(&shape_p->fill);
+  CAMLreturn(ret);
+}
+
+value caml_nsvg_shape_stroke(value shape) {
+  CAMLparam1(shape);
+  CAMLlocal1(ret);
+  NSVGshape* shape_p = (NSVGshape*) (shape & ~1);
+  ret = caml_nsvg_paint(&shape_p->stroke);
+  CAMLreturn(ret);
 }
 
 // Rasterize
